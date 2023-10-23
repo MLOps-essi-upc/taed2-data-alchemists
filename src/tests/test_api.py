d0014649657e2182012b457569181076151ab56f
marti-farre-farrus <marti.farre.farrus@estudiantat.upc.edu>
2023-10-23 17:34:54 +0200

    queralt i roger

---
+++ src/tests/test_api.py
@@ -0,0 +1,57 @@
+from http import HTTPStatus
+
+import pytest
+from fastapi.testclient import TestClient
+
+from ..src.app.api import app
+
+from PIL import Image
+
+import json
+import numpy as np
+
+
+@pytest.fixture(scope="module", autouse=True)
+def client():
+    # Use the TestClient with a `with` statement to trigger the startup and shutdown events.
+    with TestClient(app) as client:
+        return client
+
+
+@pytest.fixture
+def payload():
+    return {'../data/test/image_1.png'}
+
+
+def test_root(client):
+    response = client.get("/")
+    json = response.json()
+    assert response.status_code == 200
+    assert (
+        json["data"]["message"]
+        == "This is a digit recognizer model. Please update an image of a digit and our model will identify it!"
+    )
+    assert json["message"] == "OK"
+    assert json["status-code"] == 200
+    assert json["method"] == "GET"
+    assert json["timestamp"] is not None
+
+
+def test_model_prediction(client, payload):
+    # Use constants if fixture created
+    if os.path.isfile(payload):
+        _files = {'uploadFile': open(pyload, 'rb')}
+        # Define the URL and additional headers
+        url = "http://127.0.0.1:8000/models"
+        headers = {"accept": "application/json"}
+
+        # Send the POST request
+        response = client.post(
+            url,
+            headers=headers,
+            files=_files
+        )
+
+        assert response.status_code == 200
+    else:
+        pytest.fail("File does not exist.")
